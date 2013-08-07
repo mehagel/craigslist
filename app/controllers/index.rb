9dc284bc04a616b190e5b294756b4c3afd149dee
Apprentice <apprentice@devbootcamp.com>
2013-08-06 17:57:50 -0700

    create post and category pages

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,20 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+
+get '/posts' do
+  @posts = Post.all
+  erb :posts
+end
+
+get '/categories' do
+  @categories = Category.all
+  erb :categories
+end
+
+get '/category/:id' do
+  p params[:id]
+  p @categories = Category.find(params[:id])
+  erb :index
+end
---
+++ db/migrate/20130806163636_create_posts.rb
@@ -0,0 +1,10 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :description
+      t.string :email
+      t.string :price
+    end
+  end
+end
